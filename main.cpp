4b51fb96d4af70bbfdc391d5400afa34cb0b91a6
elrm921 <pocketrocket03@gmail.com>
2022-09-04 23:07:29 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "[IN]: ";
+
+    vector<int> numbers = {1, 1, 2, 5, 6, 1, 2, 4};
+    for (auto n : numbers) {
+        cout << n << " ";
+    }
+    cout << endl;
+
+    std::sort(numbers.begin(),numbers.end(),std::greater<int>());
+    auto last = std::unique(numbers.begin(),numbers.end());
+    numbers.erase(last,numbers.end());
+
+    cout << "[OUT]: ";
+    for (auto n : numbers) {
+        cout << n << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
